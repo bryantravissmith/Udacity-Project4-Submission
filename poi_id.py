d5a6a0db09d1b2eea9c2a14aa063c5690e478e5d
Bryan Travis Smith <bryantravissmith@gmail.com>
2015-04-07 20:02:01 -0700

    Updated Codes and Report

--- poi_id.py
+++ poi_id.py
@@ -71,6 +71,7 @@ data = pd.DataFrame.from_dict(data_dict)
 del data['TOTAL']
 del data['THE TRAVEL AGENCY IN THE PARK']
 
+#Create New Variables and use replace NaN to 0.0
 data2 = data.transpose()
 data2 = data2.replace('NaN',0.0)
 data2['from_this_person_to_poi_ratio'] = data2['from_this_person_to_poi']/data2['to_messages']
@@ -79,18 +80,21 @@ data2 = data2.replace('NaN',0.0)
 data2['poi'] = data2.poi.astype('float')
 data = data2.transpose()
 
-
+#Reset the data dictionary with total and travel agency reviewed
 data_dict = data.to_dict()
 
 data4 = data2.copy()
 data6 = data2.copy()
 
+#normalize the financial data, make two copies, and remove outliers from one copy
 for key in finance:
     data4[key] = (data2[key]-data2[key].mean())/data2[key].std()
     data6[key] = (data2[key]-data2[key].mean())/data2[key].std()
     data4 = data4[data4[key] < 3]
     data4 = data4[data4[key] > -3]
 
+
+#remove none outliers from copy to get a list of outliers
 data5 = data2.copy().transpose()
 list = data4.transpose().columns.values.tolist()
 for name in list:
@@ -144,20 +148,21 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import precision_score, recall_score
 from sklearn.cross_validation import train_test_split, KFold
+
+print "       "
+print "Exploring 2 Variable Results with 10-fold CV:"
+print "       "
+
 for i in range(len(features)):
     for j in range(i+1,len(features)):
-        #for k in range(j+1,len(features)):
-        #    for m in range(k+1,len(features)):
                 features_list = [features[i],features[j]]
-        #       features_list = [features[i],features[j],features[k],features[m]]
                 clf = DecisionTreeClassifier()
                 nb = GaussianNB()
                 temp = data2[features_list]
                 total_predictions_tree = []
                 total_predictions_nb = []
                 total_labels = []
-                kf = KFold(len(data2), n_folds=10)
-                #train_labels, test_labels, train_features, test_features = train_test_split(labels,temp.values,test_size=0.2,random_state=42)
+                kf = KFold(len(data2), n_folds=10,random_state=42)
 
                 for train_index, test_index in kf:
                     X_train, X_test = temp.values[train_index], temp.values[test_index]
@@ -172,68 +177,180 @@ for i in range(len(features)):
                 p_score = precision_score(total_predictions_tree,total_labels)
                 nb_r_score = recall_score(total_predictions_nb,total_labels)
                 nb_p_score = precision_score(total_predictions_nb,total_labels)
-                #if nb_r_score > 0.5 and nb_p_score > 0.5:
                 if nb_r_score > 0.35 and nb_p_score > 0.35:
                     print "NB",features[i],features[j], nb_r_score, nb_p_score
-                    #print "NB", features[i],features[j],features[k],features[m], nb_r_score, nb_p_score
-                #if r_score > 0.5 and p_score > 0.5:
                 if r_score > 0.35 and p_score > 0.35:
                     print "TREE",features[i],features[j], r_score, p_score
                     #print clf.feature_importances_, features[i],features[j],features[k],features[m], r_score, p_score
 
-print "    "
+print "       "
+print "Exploring 2 Variable + from_this_person_to_poi_ratio Results with 10-fold CV:"
+print "       "
+
+for i in range(len(features)):
+    for j in range(i+1,len(features)):
+                features_list = [features[i],features[j],'from_this_person_to_poi_ratio']
+                clf = DecisionTreeClassifier()
+                nb = GaussianNB()
+                temp = data2[features_list]
+                total_predictions_tree = []
+                total_predictions_nb = []
+                total_labels = []
+                kf = KFold(len(data2), n_folds=10,random_state=42)
+
+                for train_index, test_index in kf:
+                    X_train, X_test = temp.values[train_index], temp.values[test_index]
+                    y_train, y_test = labels[train_index], labels[test_index]
+                    clf.fit(X_train,y_train)
+                    nb.fit(X_train,y_train)
+                    total_predictions_tree = total_predictions_tree + clf.predict(X_test).tolist()
+                    total_predictions_nb = total_predictions_nb + nb.predict(X_test).tolist()
+                    total_labels = total_labels + y_test.tolist()
+
+                r_score = recall_score(total_predictions_tree,total_labels)
+                p_score = precision_score(total_predictions_tree,total_labels)
+                nb_r_score = recall_score(total_predictions_nb,total_labels)
+                nb_p_score = precision_score(total_predictions_nb,total_labels)
+                #if nb_r_score > 0.4 and nb_p_score > 0.4:
+                #    print "NB",features[i],features[j], nb_r_score, nb_p_score
+                if r_score > 0.4 and p_score > 0.4:
+                    print clf.feature_importances_,features[i],features[j], r_score, p_score
+                    #print clf.feature_importances_, features[i],features[j],features[k],features[m], r_score, p_score
 
 
-features2 = ['salary',
-             'to_messages',
-             'deferral_payments',
-             'total_payments',
-             'exercised_stock_options',
-             'bonus',
-             'restricted_stock',
-             'shared_receipt_with_poi',
-             'restricted_stock_deferred',
-             'total_stock_value',
-             'expenses',
-             'loan_advances',
-             'from_messages',
-             'other',
-             'from_this_person_to_poi',
-             'director_fees',
-             'deferred_income',
-             'long_term_incentive',
-             'from_poi_to_this_person']
+print "       "
+print "Exploring 2 Variable + from_poi_to_this_person_ratio Results with 10-fold CV:"
+print "       "
 
-for i in range(len(features2)):
-    for j in range(i+1,len(features2)):
-        #for k in range(j+1,len(features)):
+for i in range(len(features)):
+    for j in range(i+1,len(features)):
+                features_list = [features[i],features[j],'from_poi_to_this_person_ratio']
+                clf = DecisionTreeClassifier()
+                nb = GaussianNB()
+                temp = data2[features_list]
+                total_predictions_tree = []
+                total_predictions_nb = []
+                total_labels = []
+                kf = KFold(len(data2), n_folds=10,random_state=42)
 
-        features_list = [features2[i],features2[j],'from_this_person_to_poi_ratio','from_poi_to_this_person_ratio']
-        #features_list = [features2[i],features2[j],'from_this_person_to_poi_ratio']
-        #features_list = [features2[i],features2[j],'from_poi_to_this_person_ratio']
-        #features_list = [features[i],features[j],features[k]]
-        clf = DecisionTreeClassifier()
-        temp = data2[features_list]
-        total_predictions_tree = []
-        total_predictions_nb = []
-        total_labels = []
-        kf = KFold(len(data2), n_folds=10)
-        #train_labels, test_labels, train_features, test_features = train_test_split(labels,temp.values,test_size=0.2,random_state=42)
+                for train_index, test_index in kf:
+                    X_train, X_test = temp.values[train_index], temp.values[test_index]
+                    y_train, y_test = labels[train_index], labels[test_index]
+                    clf.fit(X_train,y_train)
+                    nb.fit(X_train,y_train)
+                    total_predictions_tree = total_predictions_tree + clf.predict(X_test).tolist()
+                    total_predictions_nb = total_predictions_nb + nb.predict(X_test).tolist()
+                    total_labels = total_labels + y_test.tolist()
 
-        for train_index, test_index in kf:
-            X_train, X_test = temp.values[train_index], temp.values[test_index]
-            y_train, y_test = labels[train_index], labels[test_index]
-            clf.fit(X_train,y_train)
-            total_predictions_tree = total_predictions_tree + clf.predict(X_test).tolist()
-            total_labels = total_labels + y_test.tolist()
+                r_score = recall_score(total_predictions_tree,total_labels)
+                p_score = precision_score(total_predictions_tree,total_labels)
+                nb_r_score = recall_score(total_predictions_nb,total_labels)
+                nb_p_score = precision_score(total_predictions_nb,total_labels)
+                #if nb_r_score > 0.4 and nb_p_score > 0.4:
+                #    print "NB",features[i],features[j], nb_r_score, nb_p_score
+                if r_score > 0.4 and p_score > 0.4:
+                    print clf.feature_importances_,features[i],features[j], r_score, p_score
+                    #print clf.feature_importances_, features[i],features[j],features[k],features[m], r_score, p_score
 
-        r_score = recall_score(total_predictions_tree,total_labels)
-        p_score = precision_score(total_predictions_tree,total_labels)
 
-        if r_score > 0.4 and p_score > 0.4:
-            print "TREE",features[i],features[j], r_score, p_score
-            #print features[i],features[j],features[k], r_score, p_score
+print "       "
+print "Exploring 2 Variable + from_this_person_to_poi_ratio + from_poi_to_this_person_ratio Results with 10-fold CV:"
+print "       "
 
+for i in range(len(features)):
+    for j in range(i+1,len(features)):
+                features_list = [features[i],features[j],'from_this_person_to_poi_ratio','from_poi_to_this_person_ratio']
+                clf = DecisionTreeClassifier()
+                nb = GaussianNB()
+                temp = data2[features_list]
+                total_predictions_tree = []
+                total_predictions_nb = []
+                total_labels = []
+                kf = KFold(len(data2), n_folds=10,random_state=42)
+
+                for train_index, test_index in kf:
+                    X_train, X_test = temp.values[train_index], temp.values[test_index]
+                    y_train, y_test = labels[train_index], labels[test_index]
+                    clf.fit(X_train,y_train)
+                    nb.fit(X_train,y_train)
+                    total_predictions_tree = total_predictions_tree + clf.predict(X_test).tolist()
+                    total_predictions_nb = total_predictions_nb + nb.predict(X_test).tolist()
+                    total_labels = total_labels + y_test.tolist()
+
+                r_score = recall_score(total_predictions_tree,total_labels)
+                p_score = precision_score(total_predictions_tree,total_labels)
+                nb_r_score = recall_score(total_predictions_nb,total_labels)
+                nb_p_score = precision_score(total_predictions_nb,total_labels)
+                #if nb_r_score > 0.4 and nb_p_score > 0.4:
+                #    print "NB",features[i],features[j], nb_r_score, nb_p_score
+                if r_score > 0.4 and p_score > 0.4:
+                    print clf.feature_importances_,features[i],features[j], r_score, p_score
+                    #print clf.feature_importances_, features[i],features[j],features[k],features[m], r_score, p_score
+
+print "       "
+print "Exploring 3 Variable with 10-fold CV:"
+print "       "
+
+for i in range(len(features)):
+    for j in range(i+1,len(features)):
+        for k in range(j+1,len(features)):
+                features_list = [features[i],features[j],features[k]]
+                clf = DecisionTreeClassifier()
+                nb = GaussianNB()
+                temp = data2[features_list]
+                total_predictions_tree = []
+                total_predictions_nb = []
+                total_labels = []
+                kf = KFold(len(data2), n_folds=10,random_state=42)
+
+                for train_index, test_index in kf:
+                    X_train, X_test = temp.values[train_index], temp.values[test_index]
+                    y_train, y_test = labels[train_index], labels[test_index]
+                    clf.fit(X_train,y_train)
+                    nb.fit(X_train,y_train)
+                    total_predictions_tree = total_predictions_tree + clf.predict(X_test).tolist()
+                    total_predictions_nb = total_predictions_nb + nb.predict(X_test).tolist()
+                    total_labels = total_labels + y_test.tolist()
+
+                r_score = recall_score(total_predictions_tree,total_labels)
+                p_score = precision_score(total_predictions_tree,total_labels)
+                nb_r_score = recall_score(total_predictions_nb,total_labels)
+                nb_p_score = precision_score(total_predictions_nb,total_labels)
+                if r_score > 0.5 and p_score > 0.5:
+                    print clf.feature_importances_,features[i],features[j],features[k], r_score, p_score
+
+print "       "
+print "Exploring 4 Variable and feature importance with 10-fold CV:"
+print "       "
+
+for i in range(len(features)):
+    for j in range(i+1,len(features)):
+        for k in range(j+1,len(features)):
+            for m in range(k+1,len(features)):
+                features_list = [features[i],features[j],features[k],features[m]]
+                clf = DecisionTreeClassifier()
+                nb = GaussianNB()
+                temp = data2[features_list]
+                total_predictions_tree = []
+                total_predictions_nb = []
+                total_labels = []
+                kf = KFold(len(data2), n_folds=10,random_state=42)
+
+                for train_index, test_index in kf:
+                    X_train, X_test = temp.values[train_index], temp.values[test_index]
+                    y_train, y_test = labels[train_index], labels[test_index]
+                    clf.fit(X_train,y_train)
+                    nb.fit(X_train,y_train)
+                    total_predictions_tree = total_predictions_tree + clf.predict(X_test).tolist()
+                    total_predictions_nb = total_predictions_nb + nb.predict(X_test).tolist()
+                    total_labels = total_labels + y_test.tolist()
+
+                r_score = recall_score(total_predictions_tree,total_labels)
+                p_score = precision_score(total_predictions_tree,total_labels)
+                nb_r_score = recall_score(total_predictions_nb,total_labels)
+                nb_p_score = precision_score(total_predictions_nb,total_labels)
+                if r_score > 0.5 and p_score > 0.5:
+                    print clf.feature_importances_, features[i],features[j],features[k],features[m], r_score, p_score
 
 
 
@@ -257,7 +374,10 @@ clf = grid_search.GridSearchCV(dt, parameters,score_func='f1')
 train_labels,test_labels,train_features,test_features = train_test_split(labels,best_features,test_size=0.50, random_state=42)
 
 clf.fit(train_features,train_labels)
-print clf.best_estimator_
+
+print "    "
+print "Running best clf in test_classifier"
+print "    "
 
 clf = clf.best_estimator_
 features_list = ['poi','expenses','other','from_this_person_to_poi']
@@ -265,5 +385,5 @@ test_classifier(clf, my_dataset, features_list)
 
 ### Dump your classifier, dataset, and features_list so
 ### anyone can run/check your results.
-dump_classifier_and_data(clf, data, features_list)
+dump_classifier_and_data(clf, my_dataset, features_list)
 
